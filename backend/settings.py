b78c83b290ecfedf9ebb29a1a9ece155830de534
Aishwaryajannu <aishwaryajannu17@gmail.com>
2025-03-04 17:55:54 +0530

    Added Render domain to ALLOWED_HOSTS

--- backend/settings.py
+++ backend/settings.py
@@ -10,7 +10,10 @@ SECRET_KEY = 'your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost",
+    "127.0.0.1",
+    "contact-form-project-1.onrender.com",
+    ]
 
 ROOT_URLCONF = 'backend.urls'
 # Installed apps
