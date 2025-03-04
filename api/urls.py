e70a5cfd5c1aa7ddd8ee1e10b50ef51b3e46e0e0
Aishwaryajannu <aishwaryajannu17@gmail.com>
2025-03-04 17:51:26 +0530

    Fixed urlpatterns definition

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import register
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-urlpatterns += [
+# Initialize urlpatterns properly before modifying it
+urlpatterns = [
     path('register/', register, name='register'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
